6993e0f82ec6e9b6c8fa6890a78ef35bd557cc0c
aareb <akchowdhury@umass.edu>
2025-03-14 01:19:09 -0400

    wrote code for navbar

---
+++ stratos/src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+// src/components/Navbar.tsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">Stratos</div>
+      <ul className="navbar-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/dashboard">Dashboard</Link></li>
+        <li><Link to="/transactions">Transactions</Link></li>
+        <li><Link to="/settings">Settings</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
